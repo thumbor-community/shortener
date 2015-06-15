15db01ed13b1d9ad0b4c7eedf8e6591aadb55a1d
Martin Samson <martin@workshopx.com>
2015-06-15 11:38:13 -0400

    Added license header.

--- tc_shortener/__init__.py
+++ tc_shortener/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2015, thumbor-community
+# Use of this source code is governed by the MIT license that can be
+# found in the LICENSE file.
+
 
 from tc_core import Extension, Extensions
 from tc_shortener.handlers.shortener import UrlShortenerHandler
--- tc_shortener/shortener.py
+++ tc_shortener/shortener.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2015, thumbor-community
+# Use of this source code is governed by the MIT license that can be
+# found in the LICENSE file.
+
 
 class Shortener(object):
 
